f092dda5ea9f8f4543d5e4620d8e0679bb6761c1
USER <blancasuarz@gmail.com>
2024-12-16 11:37:30 -0300

    Configuração inicial

---
+++ farmaciashalom/src/test/java/com/generation/farmaciashalom/FarmaciashalomApplicationTests.java
@@ -0,0 +1,13 @@
+package com.generation.farmaciashalom;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FarmaciashalomApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
